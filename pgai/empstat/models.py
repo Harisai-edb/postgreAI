7963bfab2c4d615d2d23f964cf79b69dd45b2ff2
hariiisai <harisai.58@gmail.com>
2023-09-27 16:11:06 +0530

    home, list, details pages added with functinoality

--- pgai/empstat/models.py
+++ pgai/empstat/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     department = models.CharField(max_length=50)
-    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    salary = models.IntegerField()
 
     def __str__(self):
         return self.name
--- pgai/empstat/views.py
+++ pgai/empstat/views.py
@@ -4,20 +4,178 @@ from django.db.models import Count
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+from django.apps import apps
+from io import StringIO
+from django.urls import reverse
 
+
+def home(request):
+    context = {
+        'project_title': 'PostgreAI',
+        'subtitle': 'Intelligence to Postgres',
+        'data_analysis_description': 'Data Analysis description goes here.',
+        'forecasting_description': 'Forecasting description goes here.',
+    }
+    return render(request, 'pgai/home.html', context)
+
+
+def model_list(request):
+    # Get a list of all your Django models
+    app_name = 'empstat'
+    models = apps.get_app_config(app_name).get_models() 
+    # app = apps.get_app_config(app_name)
+    # models = app.get_models()
+
+    # Create a list of dictionaries with model names and their URLs
+    model_data = []
+    for model in models:
+        model_data.append({
+            'model_name': model._meta.verbose_name,
+            'model_url':  reverse('model_details', args=[model._meta.object_name])
+        })
+    
+    # Pass the list of model names to the template
+    context = {
+        'models': model_data,
+    }
+
+    return render(request, 'pgai/model_list.html', context)
+
+def model_details(request, model_name):
+    # Find the model based on the name in the URL
+    model = apps.get_model('empstat', model_name)
+
+    # Retrieve data from the selected model and convert it to a DataFrame
+    queryset = model.objects.all().values()
+    df = pd.DataFrame.from_records(queryset)
+
+    # Calculate summary statistics (describe)
+    describe_data = df.describe()
+
+    # Capture the output of df.info() as a string
+    info_output = StringIO()
+    df.info(buf=info_output)
+    info_data = info_output.getvalue()
+    info_output.close()
+
+    # Pass the model details to the template
+    context = {
+        'model_name': model_name,
+        'data_frame': df.head().to_html(classes='table table-striped table-bordered table-sm'),
+        'describe': describe_data.to_html(classes='table table-striped table-bordered table-sm'),
+        'info': info_data,
+    }
+
+    return render(request, 'pgai/model_details.html', context)
+
+
+
+
+def data_analysis_model(request, model_name):
+    model = apps.get_model('empstat', model_name)
+    data = model.objects.all()
+    analysis_results = analyze_discreate_data_model(data)
+    context = {
+        'analysis_result': analysis_results,
+    }
+    return render(request, 'pgai/data_analysis.html', context)
+
+
+def analyze_discreate_data_model(data):
+    if not data:
+        return "No data to analyze."
+
+    columns_info = []
+    for field in data[0]._meta.get_fields():
+        column_name = field.name
+        column_type = field.get_internal_type()
+
+        # Check if the column contains discrete strings or numbers
+        is_discrete_string = False
+        is_discrete_to_show = False
+        is_number = False
+        distinct_values = list()
+        count_of_col = len(data)
+        chart_filename = 'image.png'
+
+
+        if column_type == 'CharField' or column_type == 'TextField':
+            is_discrete_string = True
+            distinct_values = data.values_list(column_name, flat=True).distinct()
+            if len(distinct_values) <= 0.4 * count_of_col:
+                is_discrete_to_show = True
+                        # Generate a bar chart for discrete columns
+                if distinct_values:
+                    plt.figure(figsize=(15, 6))
+                    plt.bar(distinct_values, [data.filter(**{column_name: val}).count() for val in distinct_values])
+                    plt.title(f'Distinct Values Count for {column_name}')
+                    plt.xlabel(column_name)
+                    plt.ylabel('Count')
+                    plt.xticks(rotation=45)
+                    plt.tight_layout()
+                    chart_filename = f'{column_name}_chart.png'
+                    plt.savefig(os.path.join('empstat/static/pgai/', chart_filename))
+                    plt.close()
+        elif column_type in ['IntegerField', 'FloatField', 'DecimalField']:
+                is_number = True
+                
+
+        columns_info.append({
+            'column_name': column_name,
+            'column_type': column_type,
+            'is_discrete_string': is_discrete_string,
+            'is_number': is_number,
+            'distinct_values': distinct_values,
+            'count_of_col': count_of_col,
+            'is_discrete_to_show': is_discrete_to_show,
+            'chart_filename': chart_filename,
+
+        })
+
+    return columns_info
+
+
+#  Old code
 
 def ml_features(request):
-    # Count of employees
-    records_count = WeatherData.objects.count()
+    app_name = 'empstat'
+    models = apps.get_app_config(app_name).get_models()
+    model_data = {}
+    # records_count = WeatherData.objects.count()
 
     # Distinct departments and their counts
-    station_city = WeatherData.objects.values('Station_City').annotate(count=Count('Station_City'))
+    # station_city = WeatherData.objects.values('Station_City').annotate(count=Count('Station_City'))
 
+    for model in models:
+        # Retrieve data from the current model and convert it to a DataFrame
+        queryset = model.objects.all().values()
+        df = pd.DataFrame.from_records(queryset)
+
+        # Calculate summary statistics (describe)
+        describe_data = df.describe()
+
+        # Capture the output of df.info() as a string
+        info_output = StringIO()
+        df.info(buf=info_output)
+        info_data = info_output.getvalue()
+        info_output.close()
+
+        # Store the data for the current model in the dictionary
+        model_name = model.__name__
+        model_data[model_name] = {
+            'data_frame': df.head().to_html(classes='table table-striped table-bordered table-sm'),
+            'describe': describe_data.to_html(classes='table table-striped table-bordered table-sm'),
+            'info': info_data,
+        }
+
+    # Pass the model data to the template
     context = {
-        'records_count': records_count,
-        'station_cities': station_city,
+        'model_data': model_data,
     }
+
     return render(request, 'pgai/ml_features.html', context)
+
+
 
 def data_analysis(request):
     data = Fuelconsumption.objects.all()
